526cfdfe82889a824c9c7fed7881db3d1b94a371
cauasauer <128698814+cauasauer@users.noreply.github.com>
2024-02-26 10:57:54 -0300

    Add files via upload

---
+++ atividade2.js
@@ -0,0 +1,13 @@
+let pessoa = {
+    nome: `Cauã Sauer`, 
+    documento: `445.679.985-00`,
+    estadocivil: `solteiro`,
+    idade: 16,
+    endereco: `Rua Florianopoles, 292, Marechal C. Rondon, Paraná`,
+}
+
+console.log("Nome: " + pessoa.nome);
+console.log("Documento: " + pessoa.documento);
+console.log("Estado Civil: " + pessoa.estadocivil);
+console.log("idade:" + pessoa.idade);
+console.log("Endereço:" + pessoa.endereco);
