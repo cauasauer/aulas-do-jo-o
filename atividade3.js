a6683c1c13923d1a623603ab35edb36a75139601
cauasauer <128698814+cauasauer@users.noreply.github.com>
2024-02-26 11:35:27 -0300

    Add files via upload

---
+++ atividade3.js
@@ -0,0 +1,8 @@
+let nome = ["Cauã", "Thales", "Dentinho", "Luiz", "Luan"]
+let idade = [16, 16, 16, 17, 17]
+
+console.log(`o ${nome[0]} tem ${idade[0]} anos`)
+console.log(`o ${nome[1]} tem ${idade[1]} anos`)   
+console.log(`o ${nome[2]} tem ${idade[2]} anos`)   
+console.log(`o ${nome[3]} tem ${idade[3]} anos`)   
+console.log(`o ${nome[4]} tem ${idade[4]} anos`)      
---
+++ atividade4.js
@@ -0,0 +1,5 @@
+//let soma = 5+9;
+//let diferenca = 5-3;
+//let produto = 5*7;
+//let quociente = 10/6;
+//let resto = 10%3;
