ba964e74d875494e5d0ef03ca7fc41b7a6991749
cauasauer <128698814+cauasauer@users.noreply.github.com>
2024-02-26 10:55:38 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,12 @@
+//let  nome = 'Cauã';
+//let idade = '16';
+//let 
+//console.log(`o seu nome é ${nome} e sua idade é ${idade}`);
+
+let nome = 'Cauã';
+let idade = 16;
+let peso = 86;
+let altura = 1.78;
+let numero = 8;
+console.log(`meu nome é ${nome} a minha idade é ${idade} minha idade mais o número é ${idade + numero} ele pesa ${peso} e sua altura é ${altura}`);
+
