f1854d1c87371c976468e1d264cb6c56d8e50e98
cauasauer <128698814+cauasauer@users.noreply.github.com>
2024-02-26 10:42:30 -0300

    Add files via upload

---
+++ boolean.js
@@ -0,0 +1,16 @@
+//let numero1 = 90;
+//let numero2 = 60;
+//console.log(`o resultado é ${numero1 == numero2}`);
+
+let cachorro = {
+    nome: `bravinho`,
+    raca: `vira-lata`,
+    porte: `pequeno`,
+    bonito: `bonito é pouco`,
+}
+console.log(cachorro)
+
+console.log(cachorro.raca)
+console.log(cachorro.nome)
+console.log(cachorro.porte)
+console.log(cachorro.bonito)
